3cda1acfd5f9545e60d712853bc8a4aa6baa77ff
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-02-24 14:11:48 +0000

    Add homepage generation logic

---
+++ src/create-home.js
@@ -0,0 +1,115 @@
+import logoBlue from "./Images/Taskie-logo-blue.png";
+import logoSky from "./Images/Taskie-logo-sky.png";
+import githubSign from "./Images/github-sign.png";
+import burgerMenu from "./Images/burger-menu.svg";
+
+function createHamburger() {
+	const projectContainer = document.querySelector("#project-container");
+	const hamburger = document.createElement("img");
+
+	projectContainer.append(hamburger);
+	hamburger.setAttribute("src", burgerMenu);
+	hamburger.classList.add("hamburger");
+
+	hamburger.addEventListener("click", () => {
+		const menuArea = document.querySelector(".menu-area");
+		menuArea.classList.toggle("hide-side-menu");
+	});
+
+	window.addEventListener("resize", () => {
+		const menuArea = document.querySelector(".menu-area");
+		if (window.innerWidth <= 768) {
+			menuArea.classList.add("hide-side-menu");
+		} else {
+			menuArea.classList.remove("hide-side-menu");
+		}
+	});
+}
+
+function createHeader() {
+	//General elements required for header
+	const projectContainer = document.querySelector("#project-container");
+	const headerContainer = document.createElement("header");
+	const logoArea = document.createElement("section");
+	const logoImg = document.createElement("img");
+	const logoText = document.createElement("p");
+	const headerMenu = document.createElement("section");
+	const gitLink = document.createElement("a");
+	const gitLogo = document.createElement("img");
+	const headerNewTaskButton = document.createElement("button");
+
+	//Build header structure
+	projectContainer.append(headerContainer);
+	headerContainer.append(logoArea, headerMenu);
+	logoArea.append(logoImg, logoText);
+	headerMenu.append(gitLink, headerNewTaskButton);
+	gitLink.append(gitLogo);
+
+	//Fill-out elements
+	logoImg.setAttribute("src", logoBlue);
+	logoText.innerText = "Just-Do-It";
+	gitLink.setAttribute(
+		"href",
+		"https://github.com/Kwadwo-Firempong-Boakye/Project-To-Do"
+	);
+	gitLink.setAttribute("target", "_blank");
+	gitLogo.setAttribute("src", githubSign);
+	headerNewTaskButton.innerHTML = `<span class="plus">+</span> Task`;
+
+	//Assign CSS classes
+	headerContainer.classList.add("header-container");
+	logoArea.classList.add("logo-area");
+	headerMenu.classList.add("header-menu");
+	headerNewTaskButton.classList.add("header-button");
+}
+
+function createDashboardStructure() {
+	//General elements required for dashboard
+	const projectContainer = document.querySelector("#project-container");
+	const dashContainer = document.createElement("main");
+	const menuArea = document.createElement("section");
+	const tasksArea = document.createElement("section");
+	const detailsArea = document.createElement("section");
+
+	//Build dashboard structure
+	projectContainer.append(dashContainer);
+	dashContainer.append(menuArea, tasksArea, detailsArea);
+
+	//Assign CSS classes
+	dashContainer.classList.add("dash-container");
+	menuArea.classList.add("menu-area");
+	tasksArea.classList.add("tasks-area");
+	detailsArea.classList.add("details-area");
+}
+
+function createSideMenu() {
+	const menuArea = document.querySelector(".menu-area");
+	const menuItems = ["All Tasks", "Due today", "Due this week", "Completed"];
+
+	menuItems.forEach((item) => {
+		let menuItemContainer = document.createElement("div");
+		let menuItemImage = document.createElementNS(
+			"http://www.w3.org/2000/svg",
+			"svg"
+		);
+		let menuItem = document.createElement("p");
+
+		menuItem.innerText = item;
+		menuItemImage.setAttribute("fill", "currentColor");
+		menuItemImage.setAttribute("viewBox", "0 0 24 24");
+		menuItemImage.innerHTML = `<path d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M10 17L5 12L6.41 10.59L10 14.17L17.59 6.58L19 8L10 17Z" />`;
+
+		menuItemContainer.classList.add("menu-item-container");
+		menuItem.classList.add("menu-item");
+
+		menuArea.append(menuItemContainer);
+		menuItemContainer.append(menuItemImage, menuItem);
+	});
+}
+
+export {
+	createHeader,
+	createDashboardStructure,
+	createHamburger,
+	createSideMenu,
+};
