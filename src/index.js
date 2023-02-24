2a3e7a980db2916c33dfdf3611c468a09685d4ee
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-02-24 14:12:36 +0000

    Add page load logic

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import './style.css';
+import "./style.css";
+import {
+	createHeader,
+	createDashboardStructure,
+	createHamburger,
+	createSideMenu,
+} from "./create-home";
+import renderDetails from "./createDetailsPanel";
+
+const createInterface = () => {
+	createHeader();
+	createHamburger();
+	createDashboardStructure();
+	createSideMenu();
+	renderDetails();
+};
+
+
+window.addEventListener("load", createInterface);
+
