2bc881aa6aa637514c611d0d1800b1c2091ba5ad
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-02-27 02:55:10 +0000

    Create sample page on load

--- src/index.js
+++ src/index.js
@@ -5,16 +5,19 @@ import {
 	createHamburger,
 	createSideMenu,
 } from "./create-home";
-import renderDetails from "./createDetailsPanel";
+import renderDetails from "./create-details-panel.js";
+import { renderTaskHeading, renderTask } from "./create-task-panel";
 
 const createInterface = () => {
 	createHeader();
 	createHamburger();
 	createDashboardStructure();
 	createSideMenu();
+	renderTaskHeading("All Tasks");
+	renderTask();
+	renderTask();
+	renderTask();
 	renderDetails();
 };
 
-
 window.addEventListener("load", createInterface);
-
