7a0f91be643a90a46339ecf6479334d480233a1d
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-03-03 23:35:45 +0000

    Complete build and style of tak input form

---
+++ src/create-task-form.js
@@ -0,0 +1,164 @@
+function taskForm() {
+	const projectContainer = document.querySelector("#project-container");
+	const projectContainerOverlay = document.createElement("div");
+	const taskFormContainer = document.createElement("div");
+	const taskFormContent = document.createElement("form");
+	const formTitleContainer = document.createElement("div");
+	const formTitle = document.createElement("p");
+	const close = document.createElement("button");
+	const taskTitleLabel = document.createElement("label");
+	const taskTitleInput = document.createElement("input");
+	const taskDescLabel = document.createElement("label");
+	const taskDescInput = document.createElement("textArea");
+	const taskDueLabel = document.createElement("label");
+	const taskDueInput = document.createElement("input");
+	const priorityFieldset = document.createElement("fieldset");
+	const priorityLegend = document.createElement("legend");
+	const taskPriorityLabelLow = document.createElement("label");
+	const taskPriorityLabelMedium = document.createElement("label");
+	const taskPriorityLabelHigh = document.createElement("label");
+	const taskPriorityInputLow = document.createElement("input");
+	const taskPriorityInputMedium = document.createElement("input");
+	const taskPriorityInputHigh = document.createElement("input");
+	const taskProjectLabel = document.createElement("label");
+	const taskProjectSelect = document.createElement("select");
+	const generalOption = document.createElement("option");
+	const submitButton = document.createElement("button");
+
+	projectContainer.append(taskFormContainer, projectContainerOverlay);
+	taskFormContainer.append(taskFormContent);
+	taskFormContent.append(
+		formTitleContainer,
+		taskTitleLabel,
+		taskDescLabel,
+		taskDueLabel,
+		priorityFieldset,
+		taskProjectLabel,
+		taskProjectSelect,
+		submitButton
+	);
+	formTitleContainer.append(formTitle, close);
+
+	formTitle.innerText = "New Task";
+	close.innerText = "X";
+	taskTitleLabel.innerText = "Task name";
+	taskDescLabel.innerText = "Task Description";
+	taskDueLabel.innerText = "Task Due By";
+	taskPriorityLabelLow.innerText = "Low";
+	taskPriorityLabelMedium.innerText = "Medium";
+	taskPriorityLabelHigh.innerText = "High";
+	priorityLegend.innerText = "Select Priority";
+	taskProjectLabel.innerText = "Choose a project";
+	generalOption.innerText = "General";
+	submitButton.innerText = "Submit Task";
+
+	taskTitleLabel.append(taskTitleInput);
+	taskDescLabel.append(taskDescInput);
+	taskDueLabel.append(taskDueInput);
+	priorityFieldset.append(
+		priorityLegend,
+		taskPriorityLabelLow,
+		// taskPriorityInputLow,
+		taskPriorityLabelMedium,
+		// taskPriorityInputMedium,
+		taskPriorityLabelHigh
+		// taskPriorityInputHigh
+	);
+	taskPriorityLabelLow.append(taskPriorityInputLow);
+	taskPriorityLabelMedium.append(taskPriorityInputMedium);
+	taskPriorityLabelHigh.append(taskPriorityInputHigh);
+	taskProjectSelect.append(generalOption);
+
+	projectContainerOverlay.classList.add("container-overlay");
+	taskFormContainer.classList.add("form-container");
+	taskFormContent.classList.add("form-content");
+	formTitleContainer.classList.add("form-title-container");
+	close.classList.add("close-button");
+	formTitle.classList.add("form-title");
+	submitButton.classList.add("submit-button");
+
+	taskTitleLabel.setAttribute("for", "task-title");
+	taskDescLabel.setAttribute("for", "task-desc");
+	taskDueLabel.setAttribute("for", "task-due");
+	taskPriorityLabelLow.setAttribute("for", "task-priority-low");
+	taskPriorityLabelMedium.setAttribute("for", "task-priority-medium");
+	taskPriorityLabelHigh.setAttribute("for", "task-priority-high");
+	taskProjectLabel.setAttribute("for", "task-project");
+
+	taskTitleInput.setAttribute("id", "task-title");
+	taskDescInput.setAttribute("id", "task-desc");
+	taskDueInput.setAttribute("id", "task-due");
+	taskPriorityInputLow.setAttribute("id", "task-priority-low");
+	taskPriorityInputMedium.setAttribute("id", "task-priority-medium");
+	taskPriorityInputHigh.setAttribute("id", "task-priority-high");
+	taskProjectSelect.setAttribute("id", "task-project");
+
+	taskTitleInput.setAttribute("name", "task-title");
+	taskDescInput.setAttribute("name", "task-desc");
+	taskDueInput.setAttribute("name", "task-due");
+	taskPriorityInputLow.setAttribute("name", "task-priority");
+	taskPriorityInputMedium.setAttribute("name", "task-priority");
+	taskPriorityInputHigh.setAttribute("name", "task-priority");
+	taskProjectSelect.setAttribute("name", "task-project");
+
+	taskTitleInput.setAttribute("type", "text");
+	taskDescInput.setAttribute("type", "textArea");
+	taskDueInput.setAttribute("type", "date");
+	taskPriorityInputLow.setAttribute("type", "radio");
+	taskPriorityInputMedium.setAttribute("type", "radio");
+	taskPriorityInputHigh.setAttribute("type", "radio");
+	submitButton.setAttribute("type", "submit");
+	taskPriorityInputLow.setAttribute("value", "low");
+	taskPriorityInputMedium.setAttribute("value", "medium");
+	taskPriorityInputMedium.setAttribute("value", "high");
+	generalOption.setAttribute("value", "General");
+
+	taskTitleInput.required = true;
+	taskProjectSelect.required = true;
+	taskPriorityInputLow.required = true;
+	close.addEventListener("click", closeForm);
+	taskFormContainer.addEventListener("click", closeForm);
+}
+
+function closeForm(e) {
+	const projectContainerOverlay = document.querySelector(".container-overlay");
+	const taskFormContainer = document.querySelector(".form-container");
+	const taskFormContent = document.querySelector(".form-content");
+	const closeButton = document.querySelector(".close-button");
+
+	if (e.target == taskFormContainer || e.target == closeButton) {
+		projectContainerOverlay.style.transform = "scale(0)";
+		taskFormContainer.style.transform = "scale(0)";
+		taskFormContent.style.transform = "scale(0)";
+
+		setTimeout(() => {
+			projectContainerOverlay.remove();
+			taskFormContainer.remove();
+			taskFormContent.remove();
+		}, 300);
+	}
+}
+
+function showForm() {
+	taskForm();
+	const projectContainerOverlay = document.querySelector(".container-overlay");
+	const taskFormContainer = document.querySelector(".form-container");
+	const taskFormContent = document.querySelector(".form-content");
+	const detailsArea = document.querySelector(".details-area");
+	const tasksArea = document.querySelector(".tasks-area");
+
+	detailsArea.classList.add("hide-details-panel");
+	tasksArea.classList.remove("no-pointer-events");
+
+	projectContainerOverlay.style.transform = "scale(0)";
+	taskFormContainer.style.transform = "scale(0)";
+	taskFormContent.style.transform = "scale(0)";
+
+	setTimeout(() => {
+		projectContainerOverlay.style.transform = "scale(1)";
+		taskFormContainer.style.transform = "scale(1)";
+		taskFormContent.style.transform = "scale(1)";
+	}, 200);
+}
+
+export { taskForm, closeForm, showForm };
