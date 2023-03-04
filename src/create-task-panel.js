8bdaef12f22db9b188aa4e445aaabbd08fee3b34
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-03-04 22:56:31 +0000

    Change behavior of detail panel opening

--- src/create-task-panel.js
+++ src/create-task-panel.js
@@ -15,6 +15,8 @@ function renderTask(
 ) {
 	const tasksArea = document.querySelector(".tasks-area");
 	const taskContainer = document.createElement("div");
+	const checkContainer = document.createElement("div");
+	const textContainer = document.createElement("div");
 	const taskCheckLabel = document.createElement("label");
 	const taskCheckInput = document.createElement("input");
 	const taskCheckSpan = document.createElement("span");
@@ -22,23 +24,37 @@ function renderTask(
 	const taskEdit = document.createElement("button");
 	const detailsArea = document.querySelector(".details-area");
 
-
 	tasksArea.append(taskContainer);
-	taskContainer.append(taskCheckLabel, taskName, taskEdit);
+	taskContainer.append(checkContainer,textContainer);
+	checkContainer.append(taskCheckLabel);
+	textContainer.append(taskName, taskEdit);
 	taskCheckLabel.append(taskCheckInput, taskCheckSpan);
 
 	taskContainer.classList.add("task-container");
+	checkContainer.classList.add("check-container");
+	textContainer.classList.add("text-container");
+	taskCheckSpan.classList.add("input-span");
 	taskCheckInput.setAttribute("type", "checkbox");
 
 	taskName.innerText = taskTitle;
 	taskEdit.innerText = "Modify";
 
-	taskContainer.addEventListener("click", () => {
-		detailsArea.classList.remove("no-display");
-		setTimeout(() => {
-			detailsArea.classList.remove("hide-details-panel");
-			tasksArea.classList.add("no-pointer-events");
-		}, 10);
+	let checkTasks = Array.from(document.querySelectorAll(".input-span"));
+	textContainer.addEventListener("click", (e) => {
+		// let isCheck = checkTasks.some((item) => item == e.target);
+		// console.log(isCheck);
+		// e.stopPropagation();
+		let modifyButton = textContainer.querySelector("button");
+		console.log(e.target);
+		if(e.target == modifyButton) {
+			return;
+		} else {
+			detailsArea.classList.remove("no-display");
+			setTimeout(() => {
+				detailsArea.classList.remove("hide-details-panel");
+				tasksArea.classList.add("no-pointer-events");
+			}, 10);
+		};
 	});
 }
 
