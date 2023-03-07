f8c7d53fe4ec6434723943b93c28112d1a9031ff
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-03-07 13:46:33 +0000

    Build pubsub architecture

---
+++ src/pub-sub.js
@@ -0,0 +1,34 @@
+class PubSub {
+	constructor() {
+		this.tracker = {
+			//key: activityName, value: [funcs]
+		};
+	}
+	subscribe(activityName, func) {
+		if (this.tracker[activityName]) {
+			this.tracker[activityName].push(func);
+		} else {
+			this.tracker[activityName] = [func];
+		}
+	}
+
+	unsubscribe(activityName, func) {
+		const funcArray = this.tracker[activityName];
+		const funcIndex = funcArray.indexOf(func);
+		if (funcIndex > -1) {
+			funcArray.splice(funcIndex, 1);
+		}
+	}
+
+	publish(activityName, ...args) {
+		if (Array.isArray(this.tracker[activityName])) {
+			this.tracker[activityName].forEach((item) => {
+				item.apply(null, args);
+			});
+		}
+	}
+}
+
+const pubSub = new PubSub();
+
+export default pubSub;
