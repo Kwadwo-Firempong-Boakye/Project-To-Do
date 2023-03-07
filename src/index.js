0856dde9b2b71fa0debb6da6a4f670f196954f28
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-03-07 13:46:13 +0000

    Remove unnecessary imports

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import {
 } from "./create-home";
 import renderDetails from "./create-details-panel.js";
 import { renderTaskHeading, renderTask } from "./create-task-panel";
-// import { showForm, taskForm } from "./create-task-form";
 
 const createInterface = () => {
 	createHeader();
