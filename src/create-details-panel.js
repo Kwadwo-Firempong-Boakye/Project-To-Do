3f57a4488b7280da538de655ba579d19d31b6aed
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-03-12 01:07:58 +0000

    Add dividers to redesigned details panel

--- src/create-details-panel.js
+++ src/create-details-panel.js
@@ -2,38 +2,48 @@ import back from "./Images/arrow.png";
 
 function renderDetails(obj) {
 	const detailsArea = document.querySelector(".details-area");
+	const divider = document.createElement("hr");
 	const detailsInfoContainer = document.createElement("div");
 	const backArrow = document.createElement("img");
 	const detailsTitle = document.createElement("h3");
+	const divider2 = document.createElement("hr");
 	const project = document.createElement("p");
 	const priority = document.createElement("p");
-	const description = document.createElement("p");
 	const dueDate = document.createElement("p");
+	const divider3 = document.createElement("hr");
+	const description = document.createElement("p");
 
-	detailsArea.append(detailsInfoContainer);
+	detailsArea.append(divider, detailsInfoContainer);
 	detailsInfoContainer.append(
 		backArrow,
 		detailsTitle,
+		divider2,
 		project,
 		priority,
-		description,
-		dueDate
+		dueDate,
+		divider3,
+		description
 	);
 
 	detailsInfoContainer.classList.add("details-info-container");
+	divider.classList.add("details-divider");
 	backArrow.classList.add("back-arrow");
 	detailsTitle.classList.add("details-title");
+	divider2.classList.add("details-divider2");
 	project.classList.add("project");
 	priority.classList.add("priority");
-	description.classList.add("task-description");
 	dueDate.classList.add("due-date");
+	divider3.classList.add("details-divider2");
+	description.classList.add("details-description");
 
 	backArrow.setAttribute("src", back);
+	detailsTitle.setAttribute("contentEditable", true);
+
 	detailsTitle.innerText = obj["name"];
-	project.innerText = obj["project"];
-	description.innerText = obj["desc"];
-	dueDate.innerHTML = `<span class="due-text">Due date</span> <br><br> ${obj["date"]}`;
-	priority.innerText = obj["priority"];
+	project.innerHTML = `<span class="project-priority-text">Project&nbsp&nbsp&nbsp|&nbsp&nbsp</span> ${obj["project"]}`;
+	priority.innerHTML = `<span class="project-priority-text">Priority&nbsp&nbsp&nbsp|&nbsp&nbsp</span> ${obj["priority"]}`;
+	dueDate.innerHTML = `<span class="project-priority-text">Due Date&nbsp&nbsp&nbsp|&nbsp&nbsp</span> ${obj["date"]}`;
+	description.innerHTML = `<span class="project-priority-text desc-title">Description</span><br>${obj["desc"]}`;
 
 	backArrow.addEventListener("click", clearDetails);
 }
@@ -42,6 +52,7 @@ const clearDetails = () => {
 	const detailsInfoContainer = document.querySelector(
 		".details-info-container"
 	);
+	const divider = document.querySelector(".details-divider");
 	const detailsArea = document.querySelector(".details-area");
 	const tasksArea = document.querySelector(".tasks-area");
 
@@ -49,6 +60,7 @@ const clearDetails = () => {
 		detailsArea.classList.add("hide-details-panel");
 		tasksArea.classList.remove("no-pointer-events");
 		setTimeout(() => {
+			divider.remove();
 			detailsInfoContainer.remove();
 		}, 250);
 	}
