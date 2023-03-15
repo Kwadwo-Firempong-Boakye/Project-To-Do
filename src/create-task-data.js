8f99143bf91557d794b10d6fbc1cd8cf53ca53bd
Kwadwo Firempong-Boakye <firempong-boakye@outlook.com>
2023-03-15 04:24:30 +0000

    Add and modify data manipulation methods
    1. Modify removeTaskData to export args to help cache project indexes
    
    2. Add function to cache indexes of project for faster loading.
    
    3. Add functions to remove & adjust cached indexes on task delete.
    
    4. Create array to store project names and function to add to said array
    
    Note- Did not use caching system due to DOM filtering solution

--- src/create-task-data.js
+++ src/create-task-data.js
@@ -1,30 +1,16 @@
 import pubSub from "./pub-sub";
 
 //Data storage array;
-let tasks = [
-	{
-		name: "Just-Do-It App - Guide",
-		desc: "Insert instruction on how to use the app",
-		date: "15/03/2023",
-		priority: "high",
-		project: "General",
-		taskId: 5000,
-	},
+const tasks = [
+	// {
+	// 	name: "Just-Do-It App - Guide",
+	// 	desc: "Insert instruction on how to use the app",
+	// 	date: "15/03/2023",
+	// 	priority: "high",
+	// 	project: "General",
+	// 	taskId: 5000,
+	// },
 ];
-
-// Function to create task ID
-const idGenerator = () => {
-	let idNumber = 100;
-	const getIdNumber = () => idNumber;
-	const increaseIdNumber = () => (idNumber += 5);
-	return {
-		getIdNumber,
-		increaseIdNumber,
-	};
-};
-
-//Instance of identity
-const taskIdentityNumber = idGenerator();
 
 //Factory function to create task item data
 const createTaskData = () => {
@@ -38,26 +24,30 @@ const createTaskData = () => {
 	const taskId = taskIdentityNumber.getIdNumber();
 
 	const taskObject = { name, desc, date, priority, project, taskId };
-
-	taskIdentityNumber.increaseIdNumber();
-	storeTaskData(taskObject);
 	pubSub.publish("task-added", taskObject);
 };
 
 //Function to store data
 const storeTaskData = (obj) => {
 	tasks.push(obj);
+	console.log(tasks);
 };
 
+//Function to remove data
 const removeTaskData = (key) => {
+	let deletedIndex;
+	let taskProject;
 	for (let i = 0; i < tasks.length; i++) {
 		if (tasks[i]["taskId"] == key) {
+			deletedIndex = i;
+			taskProject = tasks[i]["project"];
 			tasks.splice(i, 1);
-			return;
 		}
 	}
+	pubSub.publish("task-deleted", deletedIndex, taskProject);
 };
 
+//Function to modify data
 const modifyTaskData = (index) => {
 	tasks[index]["name"] = document.querySelector("#task-title").value;
 	tasks[index]["desc"] = document.querySelector("#task-desc").value;
@@ -73,4 +63,66 @@ const modifyTaskData = (index) => {
 	pubSub.publish("task-modified", idProp, nameProp);
 };
 
-export { tasks, createTaskData, removeTaskData, modifyTaskData };
+// Function to create task ID
+const idGenerator = () => {
+	let idNumber = 100;
+	const getIdNumber = () => idNumber;
+	const increaseIdNumber = () => (idNumber += 5);
+	return {
+		getIdNumber,
+		increaseIdNumber,
+	};
+};
+
+//Instance of identity
+const taskIdentityNumber = idGenerator();
+
+//Data storage for caching positions of project objects
+const projectIndexes = {};
+
+//Function to cache index of each created task by projects
+const cacheIndexes = ({ project }) => {
+	let position = tasks.length - 1;
+	if (projectIndexes[project]) {
+		projectIndexes[project].push(position);
+	} else {
+		projectIndexes[project] = [position];
+	}
+	console.log(projectIndexes);
+};
+
+//Function to correct indexes of created tasks by projects after task deletion
+const removeDeletedCacheIndex = (deletedTaskIndex, taskProject) => {
+	projectIndexes[taskProject].forEach((element, i, a) => {
+		if (element == deletedTaskIndex) {
+			a.splice(i, 1);
+			adjustCachedIndexes(deletedTaskIndex);
+			return;
+		}
+	});
+};
+
+const adjustCachedIndexes = (num) => {
+	let projects = Object.keys(projectIndexes);
+	projects.forEach((project) => {
+		projectIndexes[project] = projectIndexes[project].map((item) => {
+			return item > num ? item - 1 : item;
+		});
+	});
+	console.log(projectIndexes);
+};
+
+//Data storage for lists of projects
+const projectsUi = ["General"];
+
+const storeProjectsUi = (name) => {
+	projectsUi.push(name);
+};
+
+pubSub.subscribe("task-added", storeTaskData);
+pubSub.subscribe("task-added", cacheIndexes);
+pubSub.subscribe("task-added", taskIdentityNumber.increaseIdNumber);
+pubSub.subscribe("task-deleted", removeDeletedCacheIndex);
+pubSub.subscribe("project-ui-added", storeProjectsUi);
+
+export { tasks, createTaskData, removeTaskData, modifyTaskData, projectsUi };
